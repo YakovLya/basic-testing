60cc0cb3d8ce04b6777fc8f30127aad7f5f35868
YakovLya <yakaulyahovich@gmail.com>
2023-06-27 03:28:27 +0300

    feat: table and async tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,62 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 9, b: 5, action: Action.Add });
+    expect(result).toBe(14);
   });
 
   test('should substract two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: -5,
+      b: -12,
+      action: Action.Substract,
+    });
+    expect(result).toBe(7);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 3,
+      b: -6,
+      action: Action.Multiply,
+    });
+    expect(result).toBe(-18);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 12,
+      b: 4,
+      action: Action.Divide,
+    });
+    expect(result).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: -3,
+      b: 3,
+      action: Action.Exponentiate,
+    });
+    expect(result).toBe(-27);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 10,
+      b: 5,
+      action: 'plus',
+    });
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 'four',
+      b: 2,
+      action: Action.Multiply,
+    });
+    expect(result).toBeNull();
   });
 });
--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,20 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 5, b: 9, action: Action.Add, expected: 14 },
+  { a: -5, b: -12, action: Action.Substract, expected: 7 },
+  { a: 3, b: -6, action: Action.Multiply, expected: -18 },
+  { a: 12, b: 4, action: Action.Divide, expected: 3 },
+  { a: -3, b: 3, action: Action.Exponentiate, expected: -27 },
+  { a: 12, b: 4, action: 'plus', expected: null },
+  { a: 'four', b: 2, action: Action.Multiply, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'table test: ($a, $b, $action)',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
 });
